19c7c78e9f3182d03e978d87aa1ccdd54f4be63f
Robert Ancell <rancell@src.gnome.org>
2007-10-22 10:16:43 +0000

    Catch and ignore all exceptions writing to stdout (Bug #426461)
    svn path=/trunk/; revision=6881

--- src/lib/glchess.py
+++ src/lib/glchess.py
@@ -1,3 +1,21 @@
+import sys
+
+# Ignore any exceptions writing to stdout using print statements
+class SafeStdout:
+    def fileno(self):
+        global stdout
+        return stdout.fileno()
+
+    def write(self, data):
+        global stdout
+        try:
+            stdout.write(data)
+        except:
+            pass
+
+stdout = sys.stdout
+sys.stdout = SafeStdout()
+
 def start_game ():
 	import main
 	app = main.Application()
