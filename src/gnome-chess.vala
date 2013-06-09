GNOME/gnome-chess

commit 894a74a885ebce0d197700aa3e9b600331a18dfa

Make the computer's pieces uncontrollable
https://bugzilla.gnome.org/show_bug.cgi?id=665610

src/gnome-chess.vala
@@ -445,12 +445,21 @@ public class Application : Gtk.Application
             opponent = game.white;
             human_player = game.black;
             opponent_engine = get_engine (white_engine, white_level);
+            opponent.local_human = false;
+            human_player.local_human = true;
         }
         else if (black_engine != null)
         {
             opponent = game.black;
             human_player = game.white;
             opponent_engine = get_engine (black_engine, black_level);
+            opponent.local_human = false;
+            human_player.local_human = true;
+        }
+        else
+        {
+            game.black.local_human = true;
+            game.white.local_human = true;
         }
 
         if (opponent_engine != null)

src/chess-game.vala
@@ -13,6 +13,16 @@ public class ChessPlayer : Object
     public signal bool do_resign ();
     public signal bool do_claim_draw ();
 
+    private bool _local_human = false;
+    public bool local_human
+    {
+        get { return _local_human; }
+        set
+        {
+            _local_human = value;
+        }
+    }
+
     public ChessPlayer (Color color)
     {
         this.color = color;

src/chess-scene.vala
@@ -180,7 +180,7 @@ public class ChessScene : Object
 
     public void select_square (int file, int rank)
     {
-        if (game == null)
+        if (game == null || !game.current_player.local_human)
             return;
 
         /* Can only control when showing the current move */

commit a4b306fdb9aae7f98c90f0480f7349554a0c7857

With no engines installed, human should be able to control both players
Currently you hit a problem when you load a game that had originally
been against an engine, but have no engine installed. Fallback to human
control in this case.

Regression introduced in 894a74a885ebce0d197700aa3e9b600331a18dfa
(cherry picked from commit 79087ca62e76063011853f22ea0c1edd442f4499)

src/gnome-chess.vala
@@ -451,6 +451,7 @@ public class Application : Gtk.Application
             opponent_engine.stopped.disconnect (engine_stopped_cb);
             opponent_engine = null;
         }
+        
         if (white_engine != null)
         {
             opponent = game.white;
@@ -467,13 +468,14 @@ public class Application : Gtk.Application
             opponent.local_human = false;
             human_player.local_human = true;
         }
-        else
+
+        /* Game saved vs. human, or game saved vs. engine but none installed */
+        if (opponent_engine == null)
         {
             game.black.local_human = true;
             game.white.local_human = true;
         }
-
-        if (opponent_engine != null)
+        else
         {
             opponent_engine.ready_changed.connect (engine_ready_cb);
             opponent_engine.moved.connect (engine_move_cb);