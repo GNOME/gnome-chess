57f1931b4f7f1b73b1a0ee99ede0b9a22b507800
Michael Catanzaro <mcatanzaro@gnome.org>
2014-03-20 22:41:17 -0500

    Do not unconditionally call prctl in distributed C
    The existing check for linux/prctl.h works great if you are compiling
    the Vala code, but it's expected that users and distributions may start
    with the distributed C files instead. If the tarball is generated on
    Linux, then the call to prctl() will unconditionally appear in the
    distributed C, and if the tarball isn't generated on Linux, then it will
    be unconditionally absent.
    
    There are only two ways to fix this: (a) not distribute any C code, or
    (b) move the conditional compilation out of the Vala code. Though it
    looks like the Autotools Vala world may be starting to lean towards (a),
    let's be conservative and pick (b) for now.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=726614

---
+++ src/portability.c
@@ -0,0 +1,30 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright © 2014 Michael Catanzaro
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+ * license.
+ */
+
+#include "portability.h"
+
+#if HAVE_LINUX_PRCTL_H
+#include <signal.h>
+
+#include <linux/prctl.h>
+#include <sys/prctl.h>
+#endif
+
+/*
+ * When called in the engine process before exec, this ensures
+ * the engine is terminated in the event that we crash.
+ */
+void maybe_kill_orphan_engine ()
+{
+#if HAVE_LINUX_PRCTL_H
+    prctl (PR_SET_PDEATHSIG, SIGTERM);
+#endif
+}
