0df4b3915c6366ebc0b09136847bf2f0b1b7a004
Michael Catanzaro <mcatanzaro@gnome.org>
2015-09-05 15:04:42 -0500

    Fixup for the previous commit

--- autogen.sh
+++ autogen.sh
@@ -26,8 +26,6 @@ if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
 fi
 
 aclocal --install || exit 1
-glib-gettextize --force --copy || exit 1
-gtkdocize --copy || exit 1
 intltoolize --force --copy --automake || exit 1
 autoreconf --verbose --force --install -Wno-portability || exit 1
 
